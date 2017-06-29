2a2827f20001bd166c323793941e38c171073d7c
eliasfank <eliasfank@hotmail.com>
2017-06-29 17:39:28 -0300

    cepo

--- src/SOM_Class.java
+++ src/SOM_Class.java
@@ -2,28 +2,37 @@ import java.util.Random;
 
 
 public class SOM_Class {
-	private int mVectors;
-	private int mVecLen;
-	private int maxClusters;
-	private double minAlpha;
-	private double mAlpha;
-	private double decayRate;
-	private int mIterations;
-	private double D[];
+	private int input_set[][];
 	private double w[][];
+	private int nClusters;
+	private int vecLen;
+	private int iterationLimit;
+	private double d[];           //Network nodes.
 	
-	public SOM_Class(int numVectors, int Maximum_Clusters, double Alpha_Start, double Minimum_Alpha, double Decay_Rate, int Vector_Length)
-	{
-		mVectors = numVectors;
-		mVecLen = Vector_Length;
-		mAlpha = Alpha_Start;
-		minAlpha = Minimum_Alpha;
-		decayRate = Decay_Rate;
-		mIterations = 0;
-		maxClusters = Maximum_Clusters;
-		D = new double[maxClusters];
-		w = criaMatrizAleatorio(maxClusters, mVecLen);
-		
+	
+	private static double alpha = 0.6;
+	private static final double DECAY_RATE = 0.1;                  //About 100 iterations.
+	private static final double MIN_ALPHA = 0.01;
+	private static final double RADIUS_REDUCTION_POINT = 0.023;     //Last 20% of iterations.
+	
+	private static final int VEC_XLEN = 1;
+	private static final int VEC_YLEN = 1;
+	
+	public SOM_Class(int nClusters, int iterationLimit, int input_set[][]){
+		this.nClusters = nClusters;
+		this.input_set = input_set;
+		this.iterationLimit = iterationLimit;
+		this.d = new double[nClusters];
+		this.vecLen = input_set[0].length;
+		w = criaMatrizAleatorio(this.nClusters, this.input_set[0].length);
+//		for(int i=0; i<w.length; i++) {
+//			for(int j=0; j<w[0].length; j++) {
+//				if((j+1)%32 == 0)
+//					System.out.println();
+//				System.out.print(w[i][j]);
+//			}
+//			System.out.println();
+//		}
 		return;
 	}
 	
@@ -32,137 +41,248 @@ public class SOM_Class {
 		double w[][] = new double[maxClusters][mVecLen];
 		for(int i=0; i<w.length; i++){
 			for(int j=0; j<w[0].length; j++){
-				double randomValue = 0 + (1 - 0) * r.nextDouble();
-				w[i][j] = randomValue;
+				w[i][j] = new Random().nextDouble();
 			}
 		}
 		return w;
 	}
+	
+//	private double[][] criaMatrizAleatorio(int maxClusters, int mVecLen) {
+//		Random r = new Random();
+//		double w[][] = new double[maxClusters][mVecLen];
+//		
+//		//0
+//		for(int i = 0; i < 1024; i++)
+//			w[0][i] = (double) (this.input_set[0][i]);
+//		//1
+//		for(int i = 0; i < 1024; i++)
+//			w[1][i] = (double) (this.input_set[11][i]);
+//		//2
+//		for(int i = 0; i < 1024; i++)
+//			w[2][i] = (double) (this.input_set[5][i]);
+//		//3
+//		for(int i = 0; i < 1024; i++)
+//			w[3][i] = (double) (this.input_set[14][i]);
+//		//4
+//		for(int i = 0; i < 1024; i++)
+//			w[4][i] = (double) (this.input_set[3][i]);
+//		//5
+//		for(int i = 0; i < 1024; i++)
+//			w[5][i] = (double) (this.input_set[6][i]);
+//		//6
+//		for(int i = 0; i < 1024; i++)
+//			w[6][i] = (double) (this.input_set[4][i]);
+//		//7
+//		for(int i = 0; i < 1024; i++)
+//			w[7][i] = (double) (this.input_set[2][i]);
+//		//8
+//		for(int i = 0; i < 1024; i++)
+//			w[8][i] = (double) (this.input_set[9][i]);
+//		//9
+//		for(int i = 0; i < 1024; i++)
+//			w[9][i] = (double) (this.input_set[12][i]);
+//		
+//		return w;
+//	}
 
-	public void Train(int Patterns[][], int TrainingTests[][])
-	{
-		int Iterations = 0;
-		int i;
-		int VecNum;
-		int DMin;
-
-	    while(mAlpha > minAlpha)
+	public void train(){
+		int iterations = 0;
+	    boolean reductionFlag = false;
+	    int reductionPoint = 0;
+	    int dMin = 0;
+	    
+	    while(alpha > MIN_ALPHA)
 	    {
-	        Iterations += 1;
+	        iterations += 1;
 
-	        for(VecNum = 0; VecNum < mVectors; VecNum++)
+	        for(int vecNum = 0; vecNum < this.input_set.length; vecNum++)
 	        {
-	            // Compute input.
-	            ComputeInput(VecNum, TrainingTests);
+	            //Compute input for all nodes.
+	            computeInput(this.input_set[vecNum]);
 
-	            // See which is smaller, D(0) or D(1)?
-	            DMin = Minimum(D[0], D[1]);
-	            //System.out.println("Closest is D(" + DMin + ")");
+	            //See which is smaller?
+	            dMin = minimum(d);
 
-	            // Update the weights on the winning unit.
-	            for(i = 0; i < mVectors; i++)
-	            {
-	                w[DMin][i] = w[DMin][i] + (mAlpha * (Patterns[VecNum][i] - w[DMin][i]));
-	                //System.out.println(" w(" + i + ")= " + w[DMin][i]);
+	            //Update the weights on the winning unit.
+	            updateWeights(vecNum, dMin);
+
+	        } // VecNum
+	        
+	        //Reduce the learning rate.
+	        alpha = DECAY_RATE * alpha;
+
+	        //Reduce radius at specified point.
+	        if(alpha < RADIUS_REDUCTION_POINT){
+	            if(reductionFlag == false){
+	                reductionFlag = true;
+	                reductionPoint = iterations;
 	            }
-
 	        }
-
-	        // Reduce the learning rate.
-	        mAlpha = decayRate * mAlpha;
-
 	    }
-	    return;
-	}
-	
-	public void Test(int Patterns[][], int TrainingTests[][])
-	{
-		int i, j;
-		int VecNum;
-		int DMin;
-		java.text.DecimalFormat dfm = new java.text.DecimalFormat("###.000"); 
 
-		// Print clusters created.
-		System.out.println("Clusters for training input:");
+	    System.out.println("Iterations: " + iterations);
 		
-	    for(VecNum = 0; VecNum < mVectors; VecNum++)
-	    {
-	        // Compute input.
-	        ComputeInput(VecNum, TrainingTests);
-
-	        // See which is smaller, D(0) or D(1)?
-	        DMin = Minimum(D[0], D[1]);
-
-	        System.out.print("\nVector ( ");
-	        for(i = 0; i < mVectors; i++)
-	        {
-	        	System.out.print(Patterns[VecNum][i] + ", ");
-	        } // i
-	        System.out.println(") fits into category " + DMin);
-	    } // VecNum
-
-		// Print weight matrix.
-	    System.out.println();
-	    for(i = 0; i < maxClusters; i++)
-	    {
-	    	System.out.println("Weights for Node " + i + " connections:");
-	        for(j = 0; j < mVecLen; j++)
-	        {
-	            System.out.print(dfm.format(w[i][j]) + ", ");
-	        } // j
-	        System.out.println();
-	    } // i
-
-		// Print post-training tests.
-	    System.out.println("Categorized test input:");
-	    for(VecNum = 0; VecNum < mVectors; VecNum++)
-	    {
-	        // Compute input.
-	        ComputeInput(VecNum, TrainingTests);
-
-	        // See which is smaller, D(0) or D(1)?
-	        DMin = Minimum(D[0], D[1]);
-
-	        System.out.print("\nVector ( ");
-	        for(i = 0; i < mVectors; i++)
-	        {
-	            System.out.print(TrainingTests[VecNum][i] + ", ");
-	        } // i
-	        System.out.println(") fits into category " + DMin);
-
-	    } // VecNum
-	    return;
-	}
-	
-	private void ComputeInput(int VectorNumber, int Training_Tests[][])
-	{
-		int i, j;
+	    System.out.println("Neighborhood radius reduced after " + reductionPoint + " iterations.");
 		
-	    D[0] = 0.0;
-	    D[1] = 0.0;
-	    for(i = 0; i < maxClusters; i++)
-	    {
-	        for(j = 0; j < mVectors; j++)
-	        {
-	            D[i] += Math.pow((w[i][j] - Training_Tests[VectorNumber][j]), 2);
-	            
-	        } // j
-	    } // i
-	    return;
+		return;
 	}
 	
-	private int Minimum(double ValueA, double ValueB)
-	{
-		if(ValueA > ValueB){
-        	return 1;
-		}else{
-        	return 0;
+	 private void computeInput(int[] vectorArray)
+		{
+			clearArray(d);
+
+		    for(int i = 0; i <= (this.nClusters - 1); i++){
+		        for(int j = 0; j <= (this.vecLen - 1); j++){
+		            d[i] += Math.pow((w[i][j] - vectorArray[j]), 2);
+		        } // j
+		    } // i
+			return;
 		}
-	}
-	
-	public int Iterations()
-	{
-		return mIterations;
-	}
-	
+	    
+	    private void updateWeights(int vectorNumber, int dMin)
+		{
+	    	int y = 0;
+	    	int PointA = 0;
+	    	int PointB = 0;
+	    	boolean done = false;
+
+		    for(int i = 0; i < this.vecLen; i++)
+		    {
+		        // Only include neighbors before radius reduction point is reached.
+		        if(alpha > RADIUS_REDUCTION_POINT){
+		            y = 1;
+		            while(!done)
+		            {
+		                if(y == 1){                                   // Top row of 3.
+		                    if(dMin > VEC_XLEN - 1){
+		                        PointA = dMin - VEC_XLEN - 1;
+		                        PointB = dMin - VEC_XLEN + 1;
+		                    }else{
+		                        y = 2;
+		                    }
+		                }
+		                if(y == 2){                                   // Middle row of 3.
+		                    PointA = dMin - 1;
+		                    //DMin is like an anchor position right between these two.
+		                    PointB = dMin + 1;
+		                }
+		                if(y == 3){                                   // Bottom row of 3.
+		                    if(dMin < (VEC_XLEN * (VEC_YLEN - 1))){
+		                        PointA = dMin + VEC_XLEN - 1;
+		                        PointB = dMin + VEC_XLEN + 1;
+		                    }else{
+		                        done = true;
+		                    }
+		                }
+
+		                if(!done){
+		                    for(int DIndex = PointA; DIndex < PointB; DIndex++)
+		                    {
+		                        // Check if anchor is at left side.
+		                        if(dMin % VEC_XLEN == 0){
+		                            // Check if anchor is at top.
+		                            if(DIndex > PointA){
+		                            	w[DIndex][i] = w[DIndex][i] + (alpha * (this.input_set[vectorNumber][i] - w[DIndex][i]));
+		                            }
+		                        // Check if anchor is at right side.
+		                        }else if((dMin + 1) % VEC_XLEN == 0){
+		                            // Check if anchor is at top.
+		                            if(DIndex < PointB){
+		                                w[DIndex][i] = w[DIndex][i] + (alpha * (this.input_set[vectorNumber][i] - w[DIndex][i]));
+		                            }
+		                        // Otherwise, anchor is not at either side.
+		                        }else{
+		                            w[DIndex][i] = w[DIndex][i] + (alpha * (this.input_set[vectorNumber][i] - w[DIndex][i]));
+		                        }
+		                    } // DIndex
+		                }
+
+		                if(y == 3){
+		                    done = true;
+		                }
+		                y += 1; // prepare to start the next row.
+
+		            }
+		        }else if(alpha <= RADIUS_REDUCTION_POINT){
+		            // Update only the winner.
+		            w[dMin][i] = w[dMin][i] + (alpha * (this.input_set[vectorNumber][i] - w[dMin][i]));
+		        }
+
+		    } // i
+			return;
+		}
+	    
+	    private void clearArray(double[] nodeArray)
+		{
+			for(int i = 0; i <this.nClusters; i++)
+		    {
+		        nodeArray[i] = 0.0;
+		    } // i
+			return;
+		}
+	    
+	    private int minimum(double[] nodeArray)
+		{
+			int winner = 0;
+		    boolean foundNewWinner = false;
+		    boolean done = false;
+
+		    while(!done)
+		    {
+		        foundNewWinner = false;
+		        for(int i = 0; i <this.nClusters; i++)
+		        {
+		            if(i != winner){             //Avoid self-comparison.
+		                if(nodeArray[i] < nodeArray[winner]){
+		                    winner = i;
+		                    foundNewWinner = true;
+		                }
+		            }
+		        } // i
+
+		        if(foundNewWinner == false){
+		            done = true;
+		        }
+		    }
+		    return winner;
+		}
+
+	    
+	    public void printResults()
+	    {
+		    //int i = 0;
+		    //int j = 0;
+		    int dMin = 0;
+		
+		    //Print clusters created.
+		        System.out.println("Clusters for training input:");
+		        for(int vecNum = 0; vecNum < this.input_set.length; vecNum++)
+		        {
+		            //Compute input.
+		            computeInput(this.input_set[vecNum]);
+		
+		            //See which is smaller.
+		            dMin = minimum(d);
+		
+		            System.out.print("\nVector (");
+		            System.out.print("Pattern " + vecNum + ", " /*+ names[vecNum]*/);
+		            System.out.print(") fits into category " + dMin + "\n");
+		
+		        } // VecNum
+		    	
+		    	//The weight matrix is HUGE, and I'd found the output easier to read just by commenting out that part...
+		        //Print weight matrix.
+		        //System.out.println();
+		        //for(int i = 0; i < MAX_CLUSTERS - 1;i++)
+		        //{
+		        //    System.out.println("Weights for Node " + i + " connections:");
+		        //    for(int j = 0; j < VEC_LEN - 1; j++)
+		        //    {
+		        //        String temp = String.format("%.3f", w[i][j]);
+		        //        System.out.print(temp + ", ");
+		        //    } // j
+		        //    System.out.println("\n");
+		        //} // i
+
+	    }	
 }
--- src/SOM_Kohonen.java
+++ src/SOM_Kohonen.java
@@ -1,76 +1,84 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 
 public class SOM_Kohonen {
 	
+	public static final int MAX_ITER = 50;
 	public static final int MAX_CLUSTERS = 10;
 	public static final int VEC_LEN = 1024;
 	public static final double DECAY_RATE = 0.96;
 	public static final double MIN_ALPHA = 0.01;
-
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
+	public static final double TEST_SIZE = 0.5;  //porcentagem do arquivo de entrada utilizada para testes
+	
 	public static void main(String[] args) throws IOException {
 		
-		ArrayList<ArrayList<Integer>> cepo = new ArrayList<ArrayList<Integer>>();
-		Integer[][] cepo1; 
-		
-		
+		ArrayList<ArrayList<Integer>> input = new ArrayList<ArrayList<Integer>>();
 		String fileName = "arquivos/training";
-		
 		BufferedReader br = new BufferedReader(new FileReader(fileName));
-		
 		String linha;
 		String numero = "";
 		
 		br.readLine();
 		
-		
-		
 		while((linha = br.readLine()) != null){
-			//System.out.println(linha);
-			//System.out.println(linha.length());
-			
 			if(linha.length() == 32){
 				numero += linha;
 			}
 			if(linha.length() != 32 && numero != ""){
 				ArrayList<Integer> num = new ArrayList<Integer>();
-				
 				for (String s : numero.split("")) {
 					num.add(Integer.parseInt(s));
 				}
-				cepo.add(num);
+				input.add(num);
 				numero = "";
-				
 			}
 		}
 		
+		br.close();
+		
 		
 		int i = 0;
-		int[][] cepo_num = new int[cepo.size()][cepo.get(0).size()];
-		for (ArrayList<Integer> integers : cepo) {
+		int[][] input_num = new int[input.size()][input.get(0).size()];
+		for (ArrayList<Integer> integers : input) {
 			int j = 0;
 			for (Integer integer : integers) {
-				cepo_num[i][j] = integer;
+				input_num[i][j] = integer;
+				//System.out.println(input_num[i][j]);
 				j++;
 			}
 			i++;
 		}
 		
-		double Alpha = 0.6;
-		SOM_Class som = new SOM_Class(cepo.size(), MAX_CLUSTERS, Alpha, MIN_ALPHA, DECAY_RATE, VEC_LEN);
-		int[][] um = {cepo_num[0], cepo_num[1], cepo_num[2], cepo_num[3]};
-		int[][] dois = {cepo_num[4], cepo_num[5], cepo_num[6], cepo_num[7]};
-		som.Train(um, dois);
-		som.Test(um, dois);
+//		int limit = (int) (input_num.length*TEST_SIZE);
+//		System.out.println(limit);
+//		System.out.println(input_num.length-limit);
+//		int[][] input_train = new int[input_num.length-limit][VEC_LEN];
+//		int[][] input_test = new int[limit][VEC_LEN];
+//		
+//		for(int x=0; x<limit; x++)
+//			for(int y=0; y<input_num[0].length; y++)
+//				input_test[x][y] = input_num[x][y]; 
+//		for(int x=limit; x<input_num.length; x++)
+//			for(int y=0; y<input_num[0].length; y++)
+//				input_train[x-limit][y] = input_num[x][y]; 
+
+		
+		
+/*		for(int x=0; x<input_train.length; x++) {
+			for(int y=0; y<input_train[0].length; y++) {
+				System.out.print(input_train[x][y]);
+			}
+			System.out.println();
+		}*/
+		
+
+		SOM_Class som = new SOM_Class(MAX_CLUSTERS, MAX_ITER, input_num);
+		som.train();
+		som.printResults();
+		/*som.Train(input_train, input_test);
+		som.Test(input_train, input_test);*/
 		
 		
 
